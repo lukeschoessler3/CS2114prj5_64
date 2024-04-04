0b1a189505da87e12c75d51c41116f1cdc6ecf34
Kean Jaldin Guzman <keanjg28@vt.edu>
2024-04-04 15:29:09 -0400

    create keanjg28
    Created empty keanjg28 class file

---
+++ src/prj5/Keanjg28.java
@@ -0,0 +1,19 @@
+package prj5;
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author kjald
+ *  @version Apr 4, 2024
+ */
+public class Keanjg28
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
