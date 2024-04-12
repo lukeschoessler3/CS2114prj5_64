f42db72e5b0ac2a79cc3fc052618cd028e6dcf72
lukeschoessler3 <165323708+lukeschoessler3@users.noreply.github.com>
2024-04-11 20:47:26 -0400

    Built out Influencer and Month methods
    Added all getters and completed engagement rate methods

--- src/prj5/Influencer.java
+++ src/prj5/Influencer.java
@@ -50,4 +50,192 @@ public class Influencer
 
     // ~Public Methods ........................................................
 
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the username
+     * 
+     * @return returns the username
+     */
+    public String getUsername()
+    {
+        return username;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the channel name
+     * 
+     * @return returns the channel name
+     */
+    public String getChannelName()
+    {
+        return channelName;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the country
+     * 
+     * @return returns the country
+     */
+    public String getCountry()
+    {
+        return country;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the main topic
+     * 
+     * @return returns the main topic
+     */
+    public String getMainTopic()
+    {
+        return mainTopic;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the month array
+     * 
+     * @return returns the month array
+     */
+    public Month[] getMonthArray()
+    {
+        return monthArray;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the month at a specific index in the array
+     * 
+     * @param index
+     *            index of the month
+     * @return returns the month at a specific index
+     */
+    public Month getMonth(int index)
+    {
+        return monthArray[index];
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the first month with same name as the parameter
+     * 
+     * @param nameOfMonth
+     *            name of the requested month
+     * @return returns the first month with same name as the parameter
+     */
+    public Month getMonth(String nameOfMonth)
+    {
+        for (int i = 0; i < ARRAY_LENGTH; i++)
+        {
+            if (monthArray[i].getMonthName().equals(nameOfMonth))
+            {
+                return monthArray[i];
+            }
+        }
+        return null;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Returns the first quarter of month data
+     * 
+     * @return returns the first quarter of month data
+     */
+    public Month[] getFirstQuarter()
+    {
+        Month[] firstQuarter = new Month[3];
+
+        firstQuarter[0] = getMonth("January");
+        firstQuarter[1] = getMonth("February");
+        firstQuarter[2] = getMonth("March");
+
+        // Check that there is data for the 3 months
+        for (int i = 0; i < firstQuarter.length; i++)
+        {
+            if (firstQuarter[i] == null)
+            {
+                return null;
+            }
+        }
+
+        return firstQuarter;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Calculates the traditional engagement rate for the first quarter
+     * 
+     * @return returns the traditional engagement rate for the first quarter
+     */
+    public int traditionalEngagementRate()
+    {
+        int sumComments = 0;
+        int sumLikes = 0;
+        int sumFollowers = 0;
+        int engagementRate = 0;
+        Month[] firstQuarter = getFirstQuarter();
+
+        // Check that the first quarter is not null and if so return 0
+        if (firstQuarter == null)
+        {
+            return 0;
+        }
+
+        // Add up all values
+        for (int i = 0; i < firstQuarter.length; i++)
+        {
+            sumComments += firstQuarter[i].getComments();
+            sumLikes += firstQuarter[i].getLikes();
+            sumFollowers += firstQuarter[i].getFollowers();
+        }
+
+        engagementRate = ((sumComments + sumLikes) / sumFollowers) * 100;
+        return engagementRate;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Calculate the reach engagement rate
+     * 
+     * @return returns the reach engagement rate
+     */
+    public int reachEngagementRate()
+    {
+        int sumComments = 0;
+        int sumLikes = 0;
+        int sumViews = 0;
+        int engagementRate = 0;
+        Month[] firstQuarter = getFirstQuarter();
+
+        // Check that the first quarter is not null and if so return 0
+        if (firstQuarter == null)
+        {
+            return 0;
+        }
+
+        // Add up all values
+        for (int i = 0; i < firstQuarter.length; i++)
+        {
+            sumComments += firstQuarter[i].getComments();
+            sumLikes += firstQuarter[i].getLikes();
+            sumViews += firstQuarter[i].getViews();
+        }
+
+        engagementRate = ((sumComments + sumLikes) / sumViews) * 100;
+        return engagementRate;
+    }
+
 }
--- src/prj5/Month.java
+++ src/prj5/Month.java
@@ -22,18 +22,19 @@ public class Month
     // ----------------------------------------------------------
     /**
      * Create a new Month object.
+     * 
      * @param nameOfMonth
-     *          name of month
+     *            name of month
      * @param likeNum
-     *          number of likes
+     *            number of likes
      * @param comNum
-     *          number of comments
+     *            number of comments
      * @param viewNum
-     *          number of views
-     * @param postNum   
-     *          number of posts
+     *            number of views
+     * @param postNum
+     *            number of posts
      * @param followNum
-     *          number of followers
+     *            number of followers
      */
     public Month(
         String nameOfMonth,
@@ -52,4 +53,89 @@ public class Month
     }
     // ~Public Methods ........................................................
 
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the month name
+     * 
+     * @return returns the month name
+     */
+    public String getMonthName()
+    {
+        return monthName;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the number of likes
+     * 
+     * @return returns the number of likes
+     */
+    public int getLikes()
+    {
+        return likes;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the number of comments
+     * 
+     * @return returns the number of comments
+     */
+    public int getComments()
+    {
+        return comments;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the number of views
+     * 
+     * @return returns the number of views
+     */
+    public int getViews()
+    {
+        return views;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the number of posts
+     * 
+     * @return returns the number of posts
+     */
+    public int getPosts()
+    {
+        return posts;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the number of followers
+     * 
+     * @return returns the number of followers
+     */
+    public int getFollowers()
+    {
+        return followers;
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * 
+     * @param otherMonth
+     * @return returns -1 if less than otherMonth, 1 if greater, and 0 if they
+     *             are the same
+     */
+    public int compareTo(Month otherMonth)
+    {
+        return 0;
+    }
 }
