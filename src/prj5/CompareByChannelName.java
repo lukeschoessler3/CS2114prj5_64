8fc3f4ed2544aacf124a030509adb6ea76e7ec9b
LucasL777 <165610789+lucaslombardi2024S@users.noreply.github.com>
2024-04-17 18:50:47 -0400

    Update compareByChannelName
    a

--- src/prj5/CompareByChannelName.java
+++ src/prj5/CompareByChannelName.java
@@ -1,10 +1,22 @@
 package prj5;
-public class CompareByChannelName
-{
-    //~ Fields ................................................................
 
-    //~ Constructors ..........................................................
+import java.util.Comparator;
 
-    //~Public  Methods ........................................................
+public class CompareByChannelName implements Comparator<Influencer>{
+  
+    /*
+     * Compare two influencer's channel names by lexile order
+     * @param left the left-side to compare
+     * @para right the right-side to compare
+     * @return A negative number if the lexile order of left is
+     *         less than the right;
+     *         A positive number if the lexile order of the left
+     *         is more than the right;
+     *         0 if both lexile orders of left and right are equal
+     */
+    public int compare(Influencer left, Influencer right) {
+        
+    }
+  
 
 }
