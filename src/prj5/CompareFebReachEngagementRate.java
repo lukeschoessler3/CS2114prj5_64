6befe718d073a569bc9f117c05f843fe17c619d4
lukeschoessler3 <165323708+lukeschoessler3@users.noreply.github.com>
2024-04-26 14:06:59 -0400

    update comments

--- src/prj5/CompareFebReachEngagementRate.java
+++ src/prj5/CompareFebReachEngagementRate.java
@@ -1,5 +1,11 @@
 package prj5;
 
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times. I
+// will not lie, cheat, or steal, nor will I accept the actions of those who do.
+// -- Luke Schoessler (lukeschoessler3)
+
 import java.util.Comparator;
 
 // -------------------------------------------------------------------------
--- src/prj5/GUIInfluencerWindow.java
+++ src/prj5/GUIInfluencerWindow.java
@@ -1,5 +1,14 @@
 package prj5;
 
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times. I
+// will not lie, cheat, or steal, nor will I accept the actions of those who do.
+// -- Kean Jaldin Guzman (keanjg28)
+// -- Lucas Lombardi
+// -- Luke Schoessler
+
+
 import cs2.*;
 
 import java.awt.Color;
